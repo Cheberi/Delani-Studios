e4c0113658a4b747ff5f55441a0c31417192eb18
Cheberi <meshackkemboi011@gmail.com>
2022-01-13 15:00:05 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -93,8 +93,8 @@ $(document).ready(function(){
   $(document).ready(function(){
     $("form#form34A").submit(function(event){
       // event.preventDefault();
-      var name = $("input#inline1").val();
-      var email = $("input#inline2").val();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE2").val();
       var message = $("textarea#comment").val();
       if ($("input#MERGE1").val() && $("input#MERGE0").val()){
         alert (name + ", we have received your message. Thank you for reaching out to us.");
